fb9a94f922f688efebc3a4a588910dd976947e35
isangieri <isabelleangieri@gmail.com>
2016-01-29 10:27:43 -0800

    changed references to 'city' to 'place'

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   place = Place.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', place: Place.first)
