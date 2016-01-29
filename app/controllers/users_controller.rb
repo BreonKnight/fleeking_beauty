3140f9b245baf64af2950c71691e5d6382839ece
Breon Knight <Breonknight7@gmail.com>
2016-01-28 18:23:29 -0800

    added user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params) # calls user_params method
+    login(@user)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+end
