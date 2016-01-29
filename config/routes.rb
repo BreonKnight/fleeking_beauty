fadf40d1a3d911022a271ed288043ec67388ae01
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-29 12:20:19 -0800

    created a controller for votes, added votes to th navbar, created working votes page, added total upvotes and downvotes to user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/sessions", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
+  get "/vote", to: "votes#new"
 end
