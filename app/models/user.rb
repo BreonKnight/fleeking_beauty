5daad31e52a56ab690b49d4e62db65db02f2540f
Breon Knight <Breonknight7@gmail.com>
2016-01-28 18:33:13 -0800

    created migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
 	  @user = User.find_by({email: params[:email]})
 	  @user.try(:authenticate, params[:password])
 	end
+
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
