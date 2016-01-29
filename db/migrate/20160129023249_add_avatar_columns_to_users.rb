b6a8aaad87b7c00fba4f9b0bb1ab58aed0c33a61
Breon Knight <Breonknight7@gmail.com>
2016-01-28 18:35:06 -0800

    added logic for avatar migration

--- db/migrate/20160129023249_add_avatar_columns_to_users.rb
+++ db/migrate/20160129023249_add_avatar_columns_to_users.rb
@@ -1,4 +1,9 @@
 class AddAvatarColumnsToUsers < ActiveRecord::Migration
-  def change
-  end
+	def up
+	  add_attachment :users, :avatar
+	end
+
+	def down
+	  remove_attachment :users, :avatar
+	end
 end
