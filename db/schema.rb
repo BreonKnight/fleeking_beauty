1c1ffe444817e0f8500738df6dc1ac0c977e8c70
isangieri <isabelleangieri@gmail.com>
2016-01-29 10:05:05 -0800

    migrated places to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129023249) do
+ActiveRecord::Schema.define(version: 20160129180249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "lat"
+    t.string   "lon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
