03c28e983b87beb8e613e0068eb512a663e560c0
isangieri <isabelleangieri@gmail.com>
2016-01-29 10:16:18 -0800

    added methods index, new, create, and show for places_controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,40 @@
 class PlacesController < ApplicationController
   def index
+    @places = Place.all
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    if @place.save
+      flash[:notice] = "Successfully created #{@place.name}."
+      redirect_to place_path(@place)
+    else
+      flash[:error] = @place.errors.full_messages.join(", ")
+      redirect_to new_place_path
+    end
   end
 
   def show
+    @place = Place.find_by_id(params[:id])
   end
 
-  def edit
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :lat, :lon)
   end
 
-  def update
-  end
-
-  def destroy
-  end
 end
