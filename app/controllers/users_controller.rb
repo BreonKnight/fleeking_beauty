08a107a7b3eb7e5ec1a71a15f37c1268fc279a7b
Breon Knight <Breonknight7@gmail.com>
2016-01-28 18:40:46 -0800

    added avatar to signup and to show the avatar on the show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :userName, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :userName, :email, :avatar, :password)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129005704) do
+ActiveRecord::Schema.define(version: 20160129023249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20160129005704) do
     t.string   "last_name"
     t.string   "userName"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
