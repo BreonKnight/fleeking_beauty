87a78dfb07d7a05328283a80d7ad94707dd47b67
isangieri <isabelleangieri@gmail.com>
2016-01-29 12:27:46 -0800

    defined search and jsonToURL methods to request and parse flickr search into image urls

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,25 +1,61 @@
-require 'flickraw'
-
+require 'rubygems'
+require 'json'
+require 'net/http'
+ 
 module SearchHelper
+  def jsonToURL(data)
+    #format received:
+    # {"photos"=>
+    #   {
+    #     "page"=>1, 
+    #     "pages"=>18147, 
+    #     "perpage"=>5, 
+    #     "total"=>"90732", 
+    #     "photo"=>[{
+    #       "id"=>"24383913280", 
+    #       "owner"=>"130749742@N02", 
+    #       "secret"=>"272138f259", 
+    #       "server"=>"1592", 
+    #       "farm"=>2, 
+    #       "title"=>"2016-01-28_09-12-05", 
+    #       "ispublic"=>1, "isfriend"=>0, "isfamily"=>0
+    #     }]
+    #   }, 
+    #   "stat"=>"ok"
+    # }
 
-  def search(place)
-    # search for pictures taken within 50 miles of a place
+    #How to drill down into hash received
+    # data['photos']['photo'][0]['farm']
 
-    FlickRaw.api_key="27a19bcef1138ae6bb69dc5aba22c94e"
-    # ? FlickRaw.shared_secret="..."
+    #format required to grab flickr urls
+    # https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    
+    @urls = []
+  
+    data['photos']['photo'].each do |photo|
+      @farm_id = photo['farm']
+      @server_id = photo['server']
+      @id = photo['id']
+      @secret = photo['secret']
 
-    place = flickr.places.find :query => place.name
-    latitude = place[0]['lat'].to_f
-    longitude = place[0]['lon'].to_f
+      url = "https://farm#{@farm_id}.staticflickr.com/#{@server_id}/#{@id}_#{@secret}.jpg"
+      
+      @urls << url
+    end
 
-    # within 50 miles of a place using a bbox
-    radius = 1
-    args = {}
-    args[:bbox] = "#{longitude - radius},#{latitude - radius},#{longitude + radius},#{latitude + radius}"
+    @urls
 
-    args[:accuracy] = 1 # ? the default is street only granularity [16], which most images aren't...
-    discovered_pictures = flickr.photos.search args
-    discovered_pictures.each{|p| url = FlickRaw.url p; puts url}
+  end
+
+  def search(place) 
+    @api_key="27a19bcef1138ae6bb69dc5aba22c94e"
+    @lat = place.lat.to_f
+    @lon = place.lon.to_f
+    flickrSearch = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{@api_key}&lat=#{@lat}&lon=#{@lon}&format=json&nojsoncallback=1&page=1&per_page=5"
+    response = Net::HTTP.get_response(URI.parse(flickrSearch))
+    data = response.body
+    data = JSON.parse(data)
+    jsonToURL(data)
   end
 
 end
