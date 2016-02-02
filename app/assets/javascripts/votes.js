bcf921c8e3cd570918403bfd9d2899d4f96c56dc
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-02-01 16:59:45 -0800

    changed vote post route, have json sending back to ajax

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -2,13 +2,18 @@ $(document).ready(function() {
 	console.log("Sanity check is working!");
 
 	$('#hotbut').on("click", function (e) {
-		p_id = $('#current_img').attr('value')
-		p_url = $('#current_img').attr('src')
+		p_id = $('#current_img').attr('value');
+								// .data('place-id')
+		p_url = $('#current_img').attr('src');
 		$.ajax({
 			method: "POST",
-			url: "/vote/create",
-			data: {upvote: true, place: p_id, photourl: p_url},
+			url: "/vote",
+			// contentType: "application/json; charset=utf-8",
+			data: {upvote: true, place: p_id, photourl: "ok"},
+			dataType: 'json',
 			success: function (e) {
+				console.log(e);
+				
 			}
 		});
 	});
@@ -18,9 +23,10 @@ $(document).ready(function() {
 		p_url = $('#current_img').attr('src')
 		$.ajax({
 			method: "POST",
-			url: "/vote/create",
+			url: "/vote",
 			data: {downvote: true, place: p_id, photourl: p_url},
 			success: function (e) {
+				console.log(e);
 			}
 		});
 	});
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,23 +1,34 @@
 class VotesController < ApplicationController
   def new
-    @n = rand(0..6)
     first = Place.first
     last = Place.last
     #need to include something to test if the place id actually exists
     rand_place = rand(first.id..last.id)
     @place = Place.find_by_id(rand_place)
     @photo_array = searchFlickr(@place)
-
+    @n = rand(0..@photo_array.length)
+    respond_to do | format |
+      format.html
+      format.json do 
+        render json: {url: @photo_array[@n], place: @place.id}
+      end 
+    end
   end
 
   def create
     if(params[:upvote]) 
       Upvote.create(user_id: current_user.id, place_id: params[:place], photourl: params[:photourl])
       p "you created a new upvote"
+      redirect_to vote_path
     elsif(params[:downvote])
       Downvote.create(user_id: current_user.id, place_id: params[:place], photourl: params[:photourl])
       p "you created a new downvote"
+      redirect_to vote_path
+    else
+      redirect_to vote_path
     end
+
   end
+
 end
 
