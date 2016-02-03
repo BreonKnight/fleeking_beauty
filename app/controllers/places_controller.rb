22af74d42ee0193cf14f8f1dd82f04dbdc1c708e
isangieri <isabelleangieri@gmail.com>
2016-02-02 17:11:05 -0800

    removed uneccesary instance variables in places_controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,21 +20,13 @@ class PlacesController < ApplicationController
   end
 
   def show
-
-    #getting id for place to be shown
     @place = Place.find_by_id(params[:id])
 
     #accessing search helper to get photo for place
     @urls = searchFlickr(@place)
 
-    #gets upvotes for the place
-    @upvotes = @place.upvotes.count
-
-    #gets downvotes for the place
-    @downvotes = @place.downvotes.count
-
     #takes the upvotes and divides it by the downvotes and creates a ratio
-    @vote_percentage = calculate_upvote_percentage( @upvotes, @downvotes )
+    @vote_percentage = calculate_upvote_percentage( @place.upvotes.count, @place.downvotes.count )
 
   end
 
