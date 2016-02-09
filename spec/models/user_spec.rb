3b07682bc150ec7dd49e419ceb6173a5af4a34c3
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-08 17:49:42 -0800

    successfully added first basic user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject(:user) { FactoryGirl.create(:user) }
+  let!(:user) { FactoryGirl.create(:user) }
 
-  describe 'User Validations' do
+  describe User do
     it { should validate_presence_of(:first_name) }
-    # it { validates_presence_of(:last_name) }
-    # it { validates_presence_of(:userName) }
-    # it { validates_presence_of(:email) }
-    # it { validates_presence_of(:password) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:userName) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 
 end
