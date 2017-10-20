c65e76b96eeff1c7b7119520d1611b3d4d9e6ea6
Breon Knight <breonknight@Breons-MacBook-Pro.local>
2017-10-20 16:08:35 -0700

    added

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -1,6 +1,6 @@
 module GraphHelper
 
-#For Dryness created helper function handle downvotes and upvotes!
+#For Dryness created helper function handle downvotes and upvotes!!!
 	def countVotes(upvote, downvote)
 	  respond_to do |format|
 	    format.json {
